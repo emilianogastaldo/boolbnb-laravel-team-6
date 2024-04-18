127e3e0e7c2842013a91454700ff06a6dfb8abdd
Claudio9616 <clares9611@gmail.com>
2024-04-18 17:39:46 +0200

    add show

---
+++ resources/views/admin/flats/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h1>{{$flat->title}}</h1>
+    <p>{{$flat->description}}</p>
+    <h2>{{$flat->address}}</h2>
+    <h2>Numero stanze: {{$flat->bed}}</h2>
+    <h2>Numero bagni: {{$flat->bathroom}}</h2>
+    <h2>Metratura: {{$flat->sq_m}}</h2>
+    <figure>
+        <img src="{{asset('storage/' . $flat->image)}}" alt="{{$flat->title}}" class="img-fluid">
+    </figure>
+    {{-- <h2>{{$flat->latitude}}</h2>
+    <h2>{{$flat->longitude}}</h2> DA DECOMMENTARE QUANDO SARANNO IMPLEMNETATE --}}
+    <div class="d-flex justify-content-between">
+        <a href="{{route('admin.flats.index')}}" class="btn btn-secondary">INDIETRO</a>
+    </div>
+</div>   
+@endsection
