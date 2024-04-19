d027b1ccfdaea8190f617181fe702ab968c39f1c
Polvere243 <danielercoli@gmail.com>
2024-04-19 15:35:23 +0200

    created seeder for sponsorships and fixed migration

---
+++ database/seeders/SponsorSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sponsorship;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SponsorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // creo un array per le sponsorizzazioni
+        $sponsorships = [
+            [
+                'name'=> 'Argento',
+                'price'=> '2.99',
+                'duration'=> '24',
+            ],
+            [
+                'name'=> 'Oro',
+                'price'=> '5.99',
+                'duration'=> '72',
+            ],
+            [
+                'name'=> 'Platino',
+                'price'=> '9.99',
+                'duration'=> '144',
+            ]
+        ];
+
+        // faccio un ciclo sull'array per creare le sponsorizzazioni
+        foreach($sponsorships as $sponsorship) {
+            $new_sponsorship = new Sponsorship();
+            $new_sponsorship->fill($sponsorship);
+            $new_sponsorship->save();
+
+        }
+    }
+}
