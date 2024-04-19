89704240bb5a15e4cca986d3c5ef45e4ee0b46e4
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-19 13:46:21 +0200

    Added yields

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,30 +8,38 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    {{-- CDNS --}}
+    <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- Usando Vite -->
+    {{-- CDNS --}}
+    @yield('cdns')
+
+    {{-- Vite --}}
     @vite(['resources/js/app.js'])
 </head>
 
 <body>
     <div id="app">
 
-
+        {{-- Navbar --}}
         @include('includes.layouts.navbar')
 
+        <!-- Main -->
         <main>
+            {{-- Content --}}
             @yield('content')
         </main>
     </div>
+    
+    {{-- Scripts --}}
+    @yield('scripts')
 </body>
 
 </html>
