e57e3f7100611945de2675c1c4d9e54df9a232c4
Polvere243 <danielercoli@gmail.com>
2024-04-20 11:13:04 +0200

    wrote api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// rotte api per la home e per la show
+Route::get('/flats', [FlatController::class, 'index']);
+Route::get('/flats/{slug}', [FlatController::class, 'show']);
