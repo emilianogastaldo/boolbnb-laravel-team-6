3a7c465e857697bf85128a0a6586d533f26f0a0f
Polvere243 <danielercoli@gmail.com>
2024-04-20 11:16:40 +0200

    reordered the namespaces

--- app/Http/Controllers/Api/FlatController.php
+++ app/Http/Controllers/Api/FlatController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Flat;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Flat;
 
 class FlatController extends Controller
 {
