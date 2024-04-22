f24855230a793427cd631f7a3b342f35536d76cc
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-22 19:06:53 +0200

    fixed typos, and added a new feature

--- resources/js/image_preview.js
+++ resources/js/image_preview.js
@@ -1,16 +1,21 @@
 const placeholder = 'https://marcolanci.it/boolean/assets/placeholder.png';
 // Recupero l'input in cui carico il file dell'immagine
-const imageFiled = document.getElementById('image');
+const imageField = document.getElementById('image');
 // Recupero dove farò vedere la preview
 const previewField = document.getElementById('preview');
 
+// Recupero bottone e input-group
+const changeImageButton = document.getElementById('change-image-button');
+const previousImageField = document.getElementById('previous-image-field');
+
+
 let blobUrl;
 
-imageFiled.addEventListener('change', () => {
+imageField.addEventListener('change', () => {
     // controllo se ho il file
-    if (imageFiled.files && imageFiled.files[0]) {
+    if (imageField.files && imageField.files[0]) {
         // prendo il file
-        const file = imageFiled.files[0];
+        const file = imageField.files[0];
         //  URL temporaneo
         blobUrl = URL.createObjectURL(file);
 
@@ -26,3 +31,18 @@ imageFiled.addEventListener('change', () => {
 window.addEventListener('beforeunload', () => {
     if (blobUrl) URL.revokeObjectURL(blobUrl);
 });
+
+// Al click del bottone cambio input mostrato
+changeImageButton.addEventListener('click', () => {
+    //Aggiungo la classe d-none a previousImageField
+    previousImageField.classList.add('d-none');
+
+    //Rimuovo la classe d-none a previousImageField
+    imageField.classList.remove('d-none');
+
+    // Rimetto il placeholder
+    previewField.src = placeholder;
+
+    //Richiamo il click sul field per non dover premere "Choose File" dopo
+    imageField.click();
+})
