bebeed5026f11bd772dcec1787b325a86e57194f
emilianogastaldo <emiliano0303@gmail.com>
2024-04-22 12:41:08 +0200

    Il Grande Matteo added tomtom's style to app.scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,6 @@
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    {{-- CDN TomTom --}}
-    <link rel="stylesheet" type="text/css" href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css"/>
-
     {{-- CDNS --}}
     @yield('cdns')
 
--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~resources': '/resources/'
+            '~resources': '/resources/',
+            '~tomtom': path.resolve(__dirname, 'node_modules/@tomtom-international'),
         }
     }
 });
