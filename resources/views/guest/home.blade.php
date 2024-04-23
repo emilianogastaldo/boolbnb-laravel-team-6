5c8f0c97a8233e9804522f14bb70479c8328ba20
Polvere243 <danielercoli@gmail.com>
2024-04-23 09:54:49 +0200

    erased the carousel and created an index and a show for the guest

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,47 +6,33 @@
 
 <div class="jumbotron p-2 mb-4 rounded-3">
     <div class="container py-5">
-    <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            @forelse ($flats as $flat)   
-            <div class="carousel-item active">
-            <div class="card">
-            <div class="card-body">
-                <figure>
-                    <img src="{{ $flat->printImage() }}" alt="{{ $flat->title }}">
-                </figure>
-                <div>
-                    <h2>{{ $flat->title }}</h2>
-                    <p>{{ $flat->abstract() }}...</p>
-                </div>
-            </div>
-            </div>
-            @empty
-            <h3>Non ci sono appartamenti</h3>
-            @endforelse
-        </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
+        
     </div>
 </div>
 
 <div class="content">
     <div class="container">
-        
+        <div>
+            @forelse ($flats as $flat)   
+            <div class="card p-2 mb-3">
+                <div class="card-body">
+                    <figure>
+                        <img src="{{ $flat->printImage() }}" alt="{{ $flat->title }}">
+                    </figure>
+                    <div>
+                        <h2>{{ $flat->title }}</h2>
+                        <p>{{ $flat->abstract() }}...</p>
+                    </div>
+                </div>
+                <div class="text-end">
+                    <a class="show" href="{{ route('guest.flats.show', $flat->id) }}">Vedi dettaglio</a>
+
+                </div>
+            </div>
+            @empty
+            <h3>Non ci sono appartamenti</h3>
+            @endforelse
+        </div>
     </div>
 </div>
 @endsection
