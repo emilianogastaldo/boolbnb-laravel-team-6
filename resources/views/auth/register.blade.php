489558763f940e2a2de5913d7eb69d215d05a390
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-23 13:35:02 +0200

    Removed required attribute where is not needed, added password alert, added script for client validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,13 +9,14 @@
             <div class="card">
                 <div class="card-header">{{ __('Registrati') }}</div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" novalidate>
                         @csrf
 
+                        {{-- First Name --}}
                         <div class="mb-4 row">
                             <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
                             <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
+                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" autocomplete="first_name" autofocus>
                                 @error('first_name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -23,10 +24,12 @@
                                 @enderror
                             </div>
                         </div>
+
+                        {{-- Last Name --}}
                         <div class="mb-4 row">
                             <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
                             <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" autocomplete="last_name">
                                 @error('last_name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -35,10 +38,11 @@
                             </div>
                         </div>
                         
+                        {{-- Birth Date --}}
                         <div class="mb-4 row">
                             <label for="birth_date" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
                             <div class="col-md-3">
-                                <input id="birth_date" type="date" name="birth_date" class="form-control" required >
+                                <input id="birth_date" type="date" value="{{ old('birth_date') }}" name="birth_date" class="form-control">
                                 @error('birth_date')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -46,6 +50,8 @@
                                 @enderror
                             </div>
                         </div>
+
+                        {{-- Email --}}
                         <div class="mb-4 row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }} <span class="text-danger">*</span></label>
                             <div class="col-md-6">
@@ -58,6 +64,7 @@
                             </div>
                         </div>
 
+                        {{-- Password --}}
                         <div class="mb-4 row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} <span class="text-danger">*</span></label>
                             <div class="col-md-6">
@@ -70,16 +77,23 @@
                             </div>
                         </div>
 
+                        {{-- Password Confirm --}}
                         <div class="mb-4 row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
 
+                                {{-- Alert --}}
+                                <span role="alert">
+                                    <strong id="psw-alert" class="d-none text-danger"></strong>
+                                </span>
+                            </div>
+                       </div>
+
+                        {{-- Register Button --}}
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button id="submitButton" type="submit" class="btn btn-primary">
                                     {{ __('Registrati') }}
                                 </button>
                             </div>
@@ -90,4 +104,8 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+    @vite('resources/js/password_validation.js');
 @endsection
