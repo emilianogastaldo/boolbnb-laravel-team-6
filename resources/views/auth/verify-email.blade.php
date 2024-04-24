49ef1c1c2ddfdb992c88bd54a87a14ff24a2a249
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-24 16:23:18 +0200

    Changed language to italian

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.app')
 
+@section('title', 'Verifica Email')
+
 @section('content')
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifica il tuo indirizzo E-mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                     <div class="alert alert-success" role="alert">
-                        {{ __('A fresh verification link has been sent to your email address.') }}
+                        {{ __('Un nuovo link è stato inviato al tuo indirizzo email.') }}
                     </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Prima di continuare, perfavore controlla la tua mail per un link di verifica') }}
+                    {{ __('Se non hai ricevuto la email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Clicca qui per richiederne un\' altra') }}</button>.
                     </form>
                 </div>
             </div>
