137f23ec1de2007868cb8eef04401485e5d11266
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-24 15:53:49 +0200

    Removed invoke function and added home function and notFound function to admin HomeController

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke()
+    public function home()
     {
         return view('admin.home');
+    }
+
+    public function notFound()
+    {
+        return view('admin.not-found');
     }
 }
