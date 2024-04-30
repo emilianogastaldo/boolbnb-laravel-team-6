01a3e7a73f65892d869dc32cca12786a8981ddfc
Polvere243 <danielercoli@gmail.com>
2024-04-30 16:31:23 +0200

    forced the cancellation of a branch

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\FlatController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,6 @@ Route::get('/flats', [FlatController::class, 'index']);
 
 // Rotta per la show degli appartamenti
 Route::get('/flats/{slug}', [FlatController::class, 'show']);
+
+// rotta per scollegarsi
+Route::get('logout/', [AuthController::class, 'logout']);
