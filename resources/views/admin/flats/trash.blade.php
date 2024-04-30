26968633516ba9e64c1544377d0313c5e6745e6c
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-30 11:52:50 +0200

    Removed edit and show buttons in trash page

--- resources/views/admin/flats/trash.blade.php
+++ resources/views/admin/flats/trash.blade.php
@@ -28,16 +28,6 @@
         <td>{{$flat->bathroom}}</td>
         <td>
             <div class="d-flex justify-content-end gap-3">
-                {{-- Show --}}
-                <a href="{{route('admin.flats.show', $flat->id)}}"><i class="fa-solid fa-eye"></i></a>
-                {{-- Edit --}}
-                <a href="{{route('admin.flats.edit', $flat->id)}}"><i class="fa-solid fa-pencil"></i></a>
-                {{-- Drop --}}
-                {{-- <form action="{{route('admin.flats.drop', $flat->id)}}" method="POST">                           
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit"><i class="fa-solid fa-trash"></i></button>
-                </form> --}}
                 {{-- Restore --}}
                 <form action="{{route('admin.flats.restore', $flat->id)}}" method="POST">                           
                   @csrf
