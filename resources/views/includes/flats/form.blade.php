14d6afd8f87224a603ceeddaebd71fc98fb6771c
MatteoCantafio25 <mattyteo004@gmail.com>
2024-04-30 17:52:32 +0200

    Modified client alerts from a span to a div, changed position of the publication switch,

--- resources/views/includes/flats/form.blade.php
+++ resources/views/includes/flats/form.blade.php
@@ -16,9 +16,7 @@
                         <input type="text" class="form-control @error('title') is-invalid @elseif(old('title', '')) is-valid @enderror" name="title" id="title" value="{{old('title', $flat->title)}}">    
                         <label for="title" class="form-label">Dai un nome all'appartamento<span class="text-danger"> * </span></label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="title-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="title-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('title')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -35,9 +33,7 @@
                         <input type="text" class="form-control @error('address') is-invalid @elseif(old('address', '')) is-valid @enderror" id="input-address" value="{{old('address', $flat->address)}}">
                         <label for="address" class="form-label">Scrivi la via dell'appartamento<span class="text-danger"> *</span> (es: Via Vittorio Veneto 4, 00187 Roma)</label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="address-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="address-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('address')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -53,9 +49,7 @@
                         <input type="number" min="1" max="255" class="form-control @error('room') is-invalid @elseif(old('room', '')) is-valid @enderror" id="room" name="room" value="{{old('room', $flat->room)}}">
                         <label for="room" class="form-label">Inserisci numero stanze<span class="text-danger"> * </span></label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="room-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="room-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('room')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -66,9 +60,7 @@
                         <input type="number" min="1" max="255" class="form-control @error('bathroom') is-invalid @elseif(old('bathroom', '')) is-valid @enderror" id="bathroom" name="bathroom" value="{{old('bathroom', $flat->bathroom)}}">
                         <label for="bathroom" class="form-label">Inserisci numero bagni<span class="text-danger"> * </span></label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="bathroom-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="bathroom-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('bathroom')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -81,9 +73,7 @@
                         <input type="number" min="1" max="255" class="form-control @error('bed') is-invalid @elseif(old('bed', '')) is-valid @enderror" id="bed" name="bed" value="{{old('bed', $flat->bed)}}">
                         <label for="bed" class="form-label">Inserisci posti letto<span class="text-danger"> * </span></label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="bed-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="bed-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('bed')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -94,9 +84,7 @@
                         <input type="number" min="0" max="65535" class="form-control @error('sq_m') is-invalid @elseif(old('sq_m', '')) is-valid @enderror" id="sq_m" name="sq_m" value="{{old('sq_m', $flat->sq_m)}}">
                         <label for="sq_m">Metratura in m<sup>2</sup> <span class="text-danger"> * </span></label>
                         {{--Client Side Alert --}}
-                        <span role="alert">
-                            <strong id="sq-alert" class="d-none text-danger"></strong>
-                        </span>
+                            <div id="sq-alert" class="d-none text-danger"></div>
                         {{-- Server Side Alert --}}
                         @error('sq_m')
                         <div class="invalid-feedback">{{$message}}</div>
@@ -111,16 +99,14 @@
                 <!-- Input Change image -->
                 <div class="input-group @if(!$flat->image) d-none @endif" id="previous-image-field">
                     <button class="btn btn-outline-secondary" type="button" id="change-image-button">Cambia Immagine</button>
-                    <input type="text" class="form-control" value="{{old('image', $flat->image)}}" disabled>
+                    <input type="text" id="change-image" class="form-control" value="{{old('image', $flat->image)}}" disabled>
                 </div>
                 <!-- Input Select image -->
                 <input type="file" name="image" class="form-control @if($flat->image) d-none @endif @error('image') is-invalid @elseif(old('image', '')) is-valid @enderror" id="image" placeholder="http:// or https://">
                 {{-- Label --}}
                 <label class="mt-2" for="image">Carica un'immagine (che sia .png o .jpg) <span class="text-danger"> * </span></label>
                 {{--Client Side Alert --}}
-                <span role="alert">
-                    <strong id="image-alert" class="d-none text-danger"></strong>
-                </span>
+                    <div id="image-alert" class="d-none text-danger"></div>
                 {{-- Server Side Alert --}}
                 @error('image')
                     <div class="invalid-feedback">
@@ -139,9 +125,7 @@
                 <textarea class="form-control @error('description') is-invalid @elseif(old('description', '')) is-valid @enderror" id="description" name="description" style="height: 150px">{{old('description', $flat->description)}}</textarea>
                 <label for="description">Scrivi una descrizione dell'appartamento <span class="text-danger"> * </span></label>
                 {{--Client Side Alert --}}
-                <span role="alert">
-                    <strong id="description-alert" class="d-none text-danger"></strong>
-                </span>
+                    <div id="description-alert" class="d-none text-danger"></div>
                 {{-- Server Side Alert --}}
                 @error('description')
                 <div class="invalid-feedback">{{$message}}</div>
@@ -149,32 +133,33 @@
             </div>        
         </div>
 
-        {{-- Input bozza o pubblico --}}
-        <div class="col">
-            <div class="form-check form-switch form-check-reverse">
-                {{-- @dd($flat->is_visible) --}}
-                <input class="form-check-input" type="checkbox" role="switch" name="is_visible" id="is_visible" value="" @if (old('is_visible', $flat->is_visible)) checked @endif>
-                <label class="form-check-label" for="is_visible">Pubblicato</label>
-            </div>
-        </div>
-
         {{-- Checkbox per i servizi --}}
-        <div class="mb-3">
+        <div class="col-12">
             @foreach ($services as $service)
                 <div class="form-check form-check-inline">
                     <label class="form-check-label" for="{{"service-$service->id"}}">{{$service->name}}</label>
-                    <input class="form-check-input" type="checkbox" id="{{"service-$service->id"}}" value="{{$service->id}}" name="services[]" @if(in_array($service->id, old('services', $prev_services ?? []))) checked @endif>
+                    <input class="form-check-input services" type="checkbox" id="{{"service-$service->id"}}" value="{{$service->id}}" name="services[]" @if(in_array($service->id, old('services', $prev_services ?? []))) checked @endif>
                 </div>
-            @endforeach
-            
+                @endforeach
+                {{--Client Side Alert --}}
+                <div id="services-alert" class="d-none text-danger"></div>
             {{-- Alert Error --}}
             @error('services')
                 <div class="text-danger">{{$message}}</div>
             @enderror
         </div>
 
+        {{-- Input bozza o pubblico --}}
+        <div class="col-6">
+            <div class="form-check form-switch">
+                {{-- @dd($flat->is_visible) --}}
+                <input class="form-check-input" type="checkbox" role="switch" name="is_visible" id="is_visible" value="" @if (old('is_visible', $flat->is_visible)) checked @endif>
+                <label class="form-check-label" for="is_visible">Pubblicato</label>
+            </div>
+        </div>
+
     </div>
-    <button class="btn btn-success">Salva</button>
+    <button type="submit" class="btn btn-success">Salva</button>
     <a class="btn btn-primary" href="{{route('admin.flats.index')}}">Torna indietro</a>
 </form>
 
