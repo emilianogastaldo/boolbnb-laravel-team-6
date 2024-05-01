a5448e6c50954790fef998bdc97da2f1c06c89cd
emilianogastaldo <emiliano0303@gmail.com>
2024-05-01 23:56:04 +0200

    added user filter

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
@@ -13,8 +14,11 @@ class MessageController extends Controller
      */
     public function index()
     {
-        $user_id = auth()->user()->id;
-        $messages = Message::all();
+        $user = Auth::user();
+        $messages = Message::whereHas('flat', function ($query) use ($user) {
+            $query->where('user_id', '=', $user->id)
+                ->orderBy('created_at', 'desc');
+        })->get();
         return view('admin.messages.index', compact('messages'));
     }
 
