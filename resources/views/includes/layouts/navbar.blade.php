cfb3531dfc73cecf63022163bb7e32eee7f66f36
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-02 18:34:27 +0200

    Added way for the user to access to the sponsorships index page from the navbar

--- resources/views/includes/layouts/navbar.blade.php
+++ resources/views/includes/layouts/navbar.blade.php
@@ -21,6 +21,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('admin.messages.index') }}">{{ __('Messaggi') }}</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('admin.sponsorships.index') }}">{{ __('Sponsorizzazioni') }}</a>
+                    </li>
                 @endauth
             
             </ul>
