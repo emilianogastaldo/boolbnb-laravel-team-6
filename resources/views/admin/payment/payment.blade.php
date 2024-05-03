733161b73044df661790a885fdfab5e196b93b02
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-03 16:19:41 +0200

    Removed useless scripts

--- resources/views/admin/payment/payment.blade.php
+++ resources/views/admin/payment/payment.blade.php
@@ -2,11 +2,8 @@
 @section('title', 'Pagamento')
 
 @section('cdns')
-    {{-- <script src="https://js.braintreegateway.com/web/dropin/1.24.0/js/dropin.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> --}}
     <script src="https://js.braintreegateway.com/web/dropin/1.42.0/js/dropin.min.js"></script>
     <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 @endsection
 
 @section('content')
