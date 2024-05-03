fadef58256b2d8605a636809693013557565ab0f
emilianogastaldo <emiliano0303@gmail.com>
2024-05-03 16:15:39 +0200

    deleted card

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -35,7 +35,7 @@
           </tr>
       </thead>
       <tbody>
-          @forelse ($flats as $flat)
+        @forelse ($flats as $flat)
           <tr>
               <td>{{$flat->title}}</td>            
               <td>{{$flat->address}}</td>
@@ -67,44 +67,9 @@
                   Non ci sono messaggi
               </td>
           </tr>
-          @endforelse
+        @endforelse
       </tbody>
-  </table>
-    @forelse ($flats as $flat)
-      <div class="col mb-5">
-        <div class="card">
-            {{-- <img src="{{$flat->printImage()}}" class="card-img-top img-fluid" alt="{{$flat->name}}">--}}
-          <div class="card-body">
-            <h5 class="card-title">{{$flat->title}}</h5>
-            <p class="card-text">{{$flat->abstract()}}...</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item"><strong>INDIRIZZO: </strong>{{$flat->address}}</li>
-            <li class="list-group-item"><strong>STANZE: </strong>{{$flat->room}}</li>
-            <li class="list-group-item"><strong>LETTI: </strong>{{$flat->bed}}</li>
-            <li class="list-group-item"><strong>BAGNI: </strong>{{$flat->bathroom}}</li>
-            <li class="list-group-item"><strong>MQ: </strong>{{$flat->sq_m}}</li>
-          </ul>
-          <div class="card-body d-flex justify-content-around">
-            <a href="{{route('admin.flats.show', $flat->id)}}" class="btn btn-sm btn-primary">
-              {{-- <i class="fas fa-eye"></i> --}} VISUALIZZA
-            </a>
-            <a href="{{route('admin.flats.edit', $flat->id)}}" class="btn btn-sm btn-warning">
-              {{-- <i class="fas fa-pencil"></i> --}} MODIFICA
-            </a>
-            <form class="delete" action="{{route('admin.flats.destroy', $flat->id)}}" method="POST">                           
-              @csrf
-              @method('DELETE')
-              <button class="btn btn-sm btn-danger" type="submit">
-                {{-- <i class="fas fa-trash-can"></i> --}} ELIMINA
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    @empty
-        <h1>NON CI SONO APPARTAMENTI</h1>
-    @endforelse
+    </table>
   </div>
 </div>
 @endsection
