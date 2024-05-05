94547ed58d31e27cef55d633e081fe8f365350db
emilianogastaldo <emiliano0303@gmail.com>
2024-05-06 00:01:05 +0200

    added style

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -5,26 +5,28 @@
 @section('content')
 
 <div class="container">
-  <form class="d-flex justify-content-end align-items-center mb-5 mt-3" action="{{ route('admin.flats.index') }}" method="GET">
-    <div class="input-group w-25 p-3">
-      <input type="search" name="search" class="form-control" placeholder="Cerca..." value="{{ $search }}" autofocus>
-      <button type="submit" class="input-group-text" id="basic-addon2"><i class="fas fa-magnifying-glass"></i></button>
+  <div class="d-flex align-items-center justify-content-between my-4">
+    <form action="{{ route('admin.flats.index') }}" method="GET">
+      <div class="input-group">
+        <input type="search" name="search" class="form-control" placeholder="Cerca appartamento" value="{{ $search }}" autofocus>
+        <button type="submit" class="input-group-text" id="basic-addon2"><i class="fas fa-magnifying-glass"></i></button>
+      </div>
+    </form>
+    <div class="d-flex gap-3">
+      {{-- Go to trash page button --}}
+      <a href="{{route('admin.flats.trash')}}" class="btn btn-dark">
+        <i class="fas fa-trash-can"></i> Vedi cestino
+      </a>
+      {{-- Add flat Button --}}
+      <a href="{{route('admin.flats.create')}}" class="btn btn-success">
+        <i class="fas fa-plus me-2"></i> Aggiungi appartamento
+      </a>
     </div>
-  </form>
-  <div class="d-flex gap-3 justify-content-end p-3">
-    {{-- Go to trash page button --}}
-    <a href="{{route('admin.flats.trash')}}" class="btn btn-sm trash">
-      {{-- <i class="fas fa-trash-can"></i> --}} Vedi cestino
-    </a>
-    {{-- Add flat Button --}}
-    <a href="{{route('admin.flats.create')}}" class="btn btn-sm create">
-        {{-- <i class="fas fa-plus me-2"></i>--}} Aggiungi appartamento
-    </a>
   </div>
   <div class="table-responsive">
-    <table class="table align-middle">
+    <table class="table align-middle m-0">
       <thead>
-          <tr>
+          <tr class="text-center">
               <th scope="col">Foto</th>
               <th scope="col">Appartamento</th>
               <th scope="col">Indirizzo</th>
@@ -38,11 +40,15 @@
       </thead>
       <tbody>
         @forelse ($flats as $flat)
-          <tr >
+          <tr class="text-center">
               <td>
-                <img class="img-fluid" src="{{$flat->printImage()}}" alt="{{$flat->title}}" style="width: 200px">
+                <img class="img-fluid rounded-3" src="{{$flat->printImage()}}" alt="{{$flat->title}}" style="width: 200px">
               </td>            
-              <td>{{$flat->title}}</td>            
+              <td>{{$flat->title}}
+                @if ($flat->sponsored)
+                <span class="gold"><i class="fa-solid fa-crown"></i></span>
+                @endif              
+              </td>            
               <td>{{$flat->address}}</td>
               <td>{{$flat->is_visible ? 'Pubblico' : 'Privato'}}</td>
               <td>{{$flat->room}}</td>
@@ -51,20 +57,20 @@
               <td>{{$flat->sq_m}}</td>
               <td>
                 <div class="d-flex gap-2 flex-column align-items-center ">
-                  <a href="{{route('admin.flats.show', $flat->id)}}" class="btn btn-sm btn-primary" style="width: 92px">
+                  <a href="{{route('admin.flats.show', $flat->id)}}" class="btn btn-sm btn-primary width-92">
                     {{-- <i class="fas fa-eye"></i> --}} VISUALIZZA
                   </a>
-                  <a href="{{route('admin.flats.edit', $flat->id)}}" class="btn btn-sm btn-warning" style="width: 92px">
+                  <a href="{{route('admin.messages.flat', $flat)}}" class="btn btn-sm orange width-92" href="">MESSAGGI</a>
+                  <a href="{{route('admin.flats.edit', $flat->id)}}" class="btn btn-sm btn-warning width-92">
                     {{-- <i class="fas fa-pencil"></i> --}} MODIFICA
                   </a>
                   <form class="delete" action="{{route('admin.flats.destroy', $flat->id)}}" method="POST">                           
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-sm btn-danger" type="submit" style="width: 92px">
+                    <button class="btn btn-sm btn-danger width-92" type="submit">
                       {{-- <i class="fas fa-trash-can"></i> --}} ELIMINA
                     </button>
                   </form>
-                  <a href="{{route('admin.messages.flat', $flat)}}" class="btn btn-sm btn-info" href="" style="width: 92px">MESSAGGI</a>
                 </div>
               </td>
           </tr>
